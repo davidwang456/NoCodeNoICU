d4356c21cd9a4f89a12d0868966efa6fc64b0591
davidwang456 <329455011@qq.com>
2025-02-13 20:50:30 +0800

    excel2mysql 提交

--- Excel2Mysql/src/main/java/com/davidwang456/excel/ExcelService.java
+++ Excel2Mysql/src/main/java/com/davidwang456/excel/ExcelService.java
@@ -1,22 +1,8 @@
 package com.davidwang456.excel;
 
-import java.io.InputStream;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.excel.EasyExcel;
 @Service
-public class ExcelService {	
-  @Autowired
-  private StudentMapper studentMapper;
-
-  public void batchParseExcel2Mysql(InputStream file,String sheetName) {
-	EasyExcel.read(file, StudentDTO.class, new StudentDataListener(studentMapper)).sheet(sheetName).doRead();
-  }
-  
-  public List<StudentDTO> getStudentInfoByCondition(StudentQueryDTO studentQueryDTO){
-	 return studentMapper.getStudentInfoByCondition(studentQueryDTO);
-  }
+public class ExcelService {
+    // 保留为空，以后可以添加通用的Excel处理方法
 }
